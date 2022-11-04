fcfccb51c151b0ba5383b9b233dc6d9c877fb577
“ramaedwindap” <ramaedwindap@gmail.com>
2022-11-04 16:35:39 +0700

    contoh menggunakan komponen #basic

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ export default function App() {
     return (
         <div className='bg-slate-900 grid min-h-screen place-content-center'>
             <div className='flex gap-x-2'>
-                <button className='bg-blue-600 rounded text-white px-4 py-2'>
-                    Sign Up
-                </button>
-                <button className='bg-blue-600 rounded text-white px-4 py-2'>
-                    Sign Up
-                </button>
+                {/* Output Sign up semua! */}
+                <Button>Sign in</Button>
+                <Button>Sign up</Button>
             </div>
         </div>
+    );
+}
+
+function Button() {
+    return (
+        <button className='bg-blue-600 rounded text-white px-4 py-2'>
+            Sign Up
+        </button>
     );
 }
