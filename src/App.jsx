a4ce96ec25e4f31ea15222ee647775ca413a5b24
“ramaedwindap” <ramaedwindap@gmail.com>
2022-11-04 16:41:15 +0700

    #props1

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ export default function App() {
         <div className='bg-slate-900 grid min-h-screen place-content-center'>
             <div className='flex gap-x-2'>
                 {/* Output Sign up semua! */}
-                <Button>Sign in</Button>
-                <Button>Sign up</Button>
+                <Button text='Register' />
+                <Button text='Login' />
             </div>
         </div>
     );
 }
 
-function Button() {
+function Button(props) {
     return (
         <button className='bg-blue-600 rounded text-white px-4 py-2'>
-            Sign Up
+            {props.text}
         </button>
     );
 }
