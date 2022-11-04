3b57f176d95cf2a3ae3613561dcbd783857e4764
“ramaedwindap” <ramaedwindap@gmail.com>
2022-11-04 20:38:03 +0700

    membuat inisialisasi untuk props

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ export default function App() {
         <div className='bg-slate-900 grid min-h-screen place-content-center'>
             <div className='flex gap-x-2'>
                 {/* Output Sign up semua! */}
-                <Button>
+                <Button type='button'>
                     <IconBrandTwitter />
                     Login
                 </Button>
-                <Button>
+                <Button type='submit'>
                     <IconBrandFacebook />
                     Register
                 </Button>
@@ -19,9 +19,13 @@ export default function App() {
 }
 
 function Button(props) {
+    const { children } = props;
     return (
-        <button className='[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 bg-blue-600 rounded text-white px-4 py-2'>
-            {props.children}
+        <button
+            {...props}
+            className='[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 bg-blue-600 rounded text-white px-4 py-2'
+        >
+            {children}
         </button>
     );
 }
