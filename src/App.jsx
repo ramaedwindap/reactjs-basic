95ebdfbd364dbf0b416438e4f9937681605ef8bf
“ramaedwindap” <ramaedwindap@gmail.com>
2022-11-04 17:06:58 +0700

    menggunakan arbitary tailwind & children props pada komponen

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { IconBrandTwitter } from '@tabler/icons';
+import { IconBrandFacebook, IconBrandTwitter } from '@tabler/icons';
 
 export default function App() {
     return (
         <div className='bg-slate-900 grid min-h-screen place-content-center'>
             <div className='flex gap-x-2'>
                 {/* Output Sign up semua! */}
-                <Button text='Register' />
-                <Button
-                    text={
-                        <>
-                            <IconBrandTwitter />
-                            Login
-                        </>
-                    }
-                />
+                <Button>
+                    <IconBrandTwitter />
+                    Login
+                </Button>
+                <Button>
+                    <IconBrandFacebook />
+                    Register
+                </Button>
             </div>
         </div>
     );
@@ -21,8 +20,8 @@ export default function App() {
 
 function Button(props) {
     return (
-        <button className='bg-blue-600 rounded text-white px-4 py-2'>
-            {props.text}
+        <button className='[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 bg-blue-600 rounded text-white px-4 py-2'>
+            {props.children}
         </button>
     );
 }
