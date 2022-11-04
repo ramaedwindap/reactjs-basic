9a90698eb65561b366f1adf79f2fe08eab5980ee
“ramaedwindap” <ramaedwindap@gmail.com>
2022-11-04 16:27:34 +0700

    contoh burik, tanpa component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 export default function App() {
     return (
-        <div>
-            <h1 className='text-xl'>React</h1>
+        <div className='bg-slate-900 grid min-h-screen place-content-center'>
+            <div className='flex gap-x-2'>
+                <button className='bg-blue-600 rounded text-white px-4 py-2'>
+                    Sign Up
+                </button>
+                <button className='bg-blue-600 rounded text-white px-4 py-2'>
+                    Sign Up
+                </button>
+            </div>
         </div>
     );
 }
