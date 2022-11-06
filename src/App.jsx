d210e772976b3a988e301699e2a117134b09cd68
“ramaedwindap” <ramaedwindap@gmail.com>
2022-11-06 15:36:56 +0700

    menambahkan properti title dan footer pada komponen card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Button from './components/Button';
 import Card from './components/Card';
 
 export default function App() {
     return (
         <div className='bg-slate-100 antialiased flex items-center justify-center min-h-screen '>
             <div className='max-w-md w-full'>
-                <Card>
+                <Card
+                    title='Hello World!'
+                    footer={
+                        <div className='flex justify-end'>
+                            <Button>Register</Button>
+                        </div>
+                    }>
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia inventore porro ipsa delectus ducimus doloribus, quidem iure ex
                     labore, quae iste nemo non quia harum. Culpa ut dolorem ipsum maiores?
                 </Card>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,13 @@
-function Card({ children }) {
-    return <div>{children}</div>;
+function Card({ title, footer, children }) {
+    return (
+        <div className='shadow '>
+            <h1 className='text-2xl font-semibold'>{title}</h1>
+
+            <div className='leading-relaxed'>{children}</div>
+
+            <div className='bg-slate-50'>{footer}</div>
+        </div>
+    );
 }
 
 export default Card;
