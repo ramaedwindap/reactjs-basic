24aa3edb990611b77dedb5d15c4d1e84e58e19ba
“ramaedwindap” <ramaedwindap@gmail.com>
2022-11-06 15:07:48 +0700

    merubah style penulisan value fungsi variabel props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,8 @@
 import clsx from 'clsx';
 
-export default function Button(props) {
-    const { className = 'bg-blue-600', children, type = 'submit' } = props;
+export default function Button({ className = 'bg-blue-600', children, type = 'submit', ...props }) {
     return (
         <button
-            {...props}
             type={type}
             className={clsx(className, '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 rounded text-white px-4 py-2')}>
             {children}
