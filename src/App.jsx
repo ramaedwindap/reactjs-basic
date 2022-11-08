21818b206a3b2000a308a76ecffe8711af9c73c9
“ramaedwindap” <ramaedwindap@gmail.com>
2022-11-08 13:10:37 +0700

    membuat simple component usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import PlaceContentCenter from './components/PlaceContentCenter';
 import Counter from './components/Counter';
-import { useState } from 'react';
 
 export default function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <PlaceContentCenter>
-            <Counter count={count} setCount={setCount} />
+            <Counter />
         </PlaceContentCenter>
     );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import Button from './Button';
 
-export default function Counter({ count, setCount }) {
+export default function Counter() {
+    const [count, setCount] = useState(0);
+
     function handleClick() {
         setCount((x) => x + 1);
     }
