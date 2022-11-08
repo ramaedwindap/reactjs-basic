d1145102fc9709a1e35221fe386485d6bf4e306c
“ramaedwindap” <ramaedwindap@gmail.com>
2022-11-08 13:12:08 +0700

    membuat initialValue pada component usestate

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Counter from './components/Counter';
 export default function App() {
     return (
         <PlaceContentCenter>
-            <Counter />
+            <Counter initialValue={10} />
         </PlaceContentCenter>
     );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import Button from './Button';
 
-export default function Counter() {
-    const [count, setCount] = useState(0);
+export default function Counter({ initialValue }) {
+    const [count, setCount] = useState(initialValue);
 
     function handleClick() {
         setCount((x) => x + 1);
